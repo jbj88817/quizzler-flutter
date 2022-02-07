afa01a76ab77df4ac09221f607cc76b314ac825c
Bojie Jiang <jiangbojie@gmail.com>
2022-02-06 16:30:27 -0800

    Question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,12 +27,16 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionBank = [
+    Question(
+        questionText: 'You can lead a cow down stairs but not up stairs.',
+        questionAnswer: false),
+    Question(
+        questionText:
+            'Approximately one quarter of human bones are in the feet.',
+        questionAnswer: true),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
   ];
-  List<bool> answers = [false, true, true];
   int questionNumber = 0;
 
   @override
@@ -46,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber % questions.length],
+                questionBank[questionNumber % questionBank.length].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,7 +77,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  if (answers[questionNumber % questions.length]) {
+                  if (questionBank[questionNumber % questionBank.length]
+                      .questionAnswer) {
                     scoreKeeper.add(Icon(
                       Icons.check,
                       color: Colors.green,
@@ -104,7 +110,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 setState(() {
-                  if (!answers[questionNumber % questions.length]) {
+                  if (!questionBank[questionNumber % questionBank.length]
+                      .questionAnswer) {
                     scoreKeeper.add(Icon(
                       Icons.check,
                       color: Colors.green,
